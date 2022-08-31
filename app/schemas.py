858a2ab8b9b903cd2a9ec1fa899edabd031db447
Vuri Erdem <vuri@MacBook-Pro-von-Vuri.local>
2022-08-31 12:56:49 +0200

    added create_user()

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class PostBase(BaseModel):
@@ -17,6 +17,11 @@ class Post(PostBase):
     
     class Config:
         orm_mode = True
+        
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    
         
         
 

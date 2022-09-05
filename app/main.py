9ed5b273be06d2a6d7218aee8b9725a9682fb174
LinuxVuri <vuri.erdem@gmail.com>
2022-09-05 19:06:01 +0200

    added oauth2.py for authentication

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ import time
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import post, user
+from .routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -45,6 +45,7 @@ def find_index(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 

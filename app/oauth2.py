959272062df435342fa513f2decdfddd70d5fa1d
Vuri Erdem <vuri@MacBook-Pro-von-Vuri.local>
2022-09-10 13:58:14 +0200

    finished authentication

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 #Secret_Key
 #Algorithm
@@ -13,9 +18,34 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_acces_token(data: dict):
     to_encode = data.copy()
     
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     
     encoded_jwt = jwt.encode(to_encode, SECRETE_KEY, algorithm=ALGORITHM)
+    
     return encoded_jwt
+
+
+
+def verify_access_token(token: str, credentials_exceptions):
+    
+    try:
+        payload = jwt.decode(token, SECRETE_KEY, algorithms=[ALGORITHM])
+
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exceptions
+        token_data = schemas.TokenData(id=id)
+        
+    except JWTError:
+        raise credentials_exceptions
+    
+    return token_data
+    
+    
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Baerer"})
+    
+    return verify_access_token(token, credentials_exception)
     

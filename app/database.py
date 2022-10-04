7bd168a12f16c2cb298d26a2fcb97d77596a831a
Vuri Erdem <vuri@MacBook-Pro-von-Vuri.local>
2022-10-04 17:59:25 +0200

    removed unnecessary files

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:4133@localhost/fastapi'
@@ -18,3 +21,17 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+        
+#Connecting raw with psycopg2 and without SQLAlchemy
+"""while True:
+        
+    try:
+        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='4133', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was succesfull!")
+        break
+    except Exception as error:
+        print("Connecting to Database failed")
+        print("Error: ", error)
+        time.sleep(2)"""
--- app/main.py
+++ app/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
 from . import models
 from .database import engine
 from .routers import post, user, auth
@@ -12,17 +9,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
     
-while True:
-        
-    try:
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='4133', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to Database failed")
-        print("Error: ", error)
-        time.sleep(2)
+
     
     
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2 }]

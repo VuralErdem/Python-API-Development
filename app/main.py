6a155814d673edd3f60144cb88f280df80e83979
Vuri Erdem <vuri@MacBook-Pro-von-Vuri.local>
2022-10-09 15:43:41 +0200

    added .gitignore and created .env and config.py

--- app/main.py
+++ app/main.py
@@ -1,41 +1,20 @@
+from distutils.command.config import config
 from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth
+from .config import settings
 
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-    
-
-    
-    
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2 }]
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
         
-        
-        
-def find_index(id):
-    for i, p in enumerate(my_posts):
-        if p["id"] == id:
-            return i
-        
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
-
-
-@app.get("/")
-def root():
-    return {"message": "welcome to my api????"}
 
 
     

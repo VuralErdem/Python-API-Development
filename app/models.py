d98f36a8a24b5110c1f7f5b2fa8367ca2beda11d
Vuri Erdem <vuri@MacBook-Pro-von-Vuri.local>
2022-10-10 14:43:07 +0200

    added vote table

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
+
 class Post(Base):
     __tablename__ = "posts"
     
@@ -26,6 +27,15 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), 
                         nullable=False, server_default=text('now()'))
+    
+    
+
+class Vote(Base):
+    __tablename__ = "votes"
+    
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+    
     
     
 

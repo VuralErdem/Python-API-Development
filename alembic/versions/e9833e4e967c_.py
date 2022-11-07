818b7461497f94531da77532625c0d8d6a410cbb
Vuri Erdem <vuri@MacBook-Pro-von-Vuri.local>
2022-11-07 09:47:35 +0100

    added requirements.txt

--- alembic/versions/e9833e4e967c_.py
+++ alembic/versions/e9833e4e967c_.py
@@ -5,6 +5,7 @@ Revises: 1175be247a2f
 Create Date: 2022-10-24 10:19:29.677205
 
 """
+from xmlrpc.client import Boolean
 from alembic import op
 import sqlalchemy as sa
 
@@ -16,10 +17,14 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade() -> None:
-    op.add_column("posts" sa)
-    pass
+def upgrade():
+    op.add_column("posts", sa.Column(
+        "published", sa.Boolean(), nullable=False, server_default="TRUE"),)
+    op.add_column("posts", sa.Column(
+        "created_at", sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text("NOW()")),)
+    
 
 
-def downgrade() -> None:
-    pass
+def downgrade():
+    op.drop_column("posts", "published")
+    op.drop_column("posts", "created_at")
